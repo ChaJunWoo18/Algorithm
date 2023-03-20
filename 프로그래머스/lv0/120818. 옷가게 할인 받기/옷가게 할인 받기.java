c3cc5c78cf60cc0d0c9ac443cb0b6c00335d1d05
ChaJunWoo18 <105590071+ChaJunWoo18@users.noreply.github.com>
2023-03-20 16:23:48 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        double a = 0;
+        if (price >= 500000)
+            a = price * 0.8;
+        else if (price >= 300000 && price <500000)
+            a = price * 0.9;
+        else if (price >= 100000 && price <300000)
+            a = price * 0.95;
+        else
+            a = price;
+        answer = (int)a;
+        return answer;
+    }
+}
