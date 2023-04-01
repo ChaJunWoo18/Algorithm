65536a02b97ff7b7db09dd204e5207cc57b9ec86
ChaJunWoo18 <105590071+ChaJunWoo18@users.noreply.github.com>
2023-04-02 03:02:18 +0900

    [level 0] Title: 숫자 찾기, Time: 0.10 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int num, int k) {
+        int answer = 0;
+        String [] arr = Integer.toString(num).split("");
+        String a = Integer.toString(k);
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i].equals(a)) {
+                answer = i+1;
+                break;
+            }
+        }
+        if(answer ==0)
+            answer = -1;
+        return answer;
+    }
+}
