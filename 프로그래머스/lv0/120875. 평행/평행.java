403e0a79b607af81b1b73868b170dd117b17e4b1
ChaJunWoo18 <105590071+ChaJunWoo18@users.noreply.github.com>
2023-04-07 02:56:06 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[][] dots) {
+        int answer = 0;
+        for(int i =0; i<dots.length;i++) {
+            double temp =기울기(dots[i],dots[(i+1)%4]);
+            double temp2 =기울기(dots[(i+2)%4],dots[(i+3)%4]);
+            if(temp==temp2) {
+                answer = 1;
+            }
+        }
+        return answer;
+    }
+    public static float 기울기(int[]a1,int[]a2) {
+        float 분모,분자;
+            분모= a1[0]-a2[0];
+            분자= a1[1]-a2[1];
+        return 분자/분모;
+    }
+}
