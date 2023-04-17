5fbafee3988eace9bfcb935e2e3843c50fe95745
ChaJunWoo18 <105590071+ChaJunWoo18@users.noreply.github.com>
2023-04-18 02:54:29 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.10 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String[] arr = Integer.toString(n).split("");
+        for(String s:arr) 
+            answer += Integer.parseInt(s);
+        
+        return answer;
+    }
+}
