1aecf3a698c51a7b31e4934e254c24ca96040a4b
ChaJunWoo18 <105590071+ChaJunWoo18@users.noreply.github.com>
2024-04-21 03:39:10 +0900

    [level 2] Title: 짝지어 제거하기, Time: 38.81 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack stack = new Stack();
+        stack.push(s.charAt(0));
+        System.out.println(stack.peek());
+        for(int i=1;i<s.length();i++) {
+            char c = s.charAt(i);
+            if(!stack.isEmpty() && stack.peek()==c){
+                stack.pop();
+                continue;
+            }
+            else{
+                stack.push(c);
+            }
+        }
+        if(stack.isEmpty())
+            return 1;
+        return 0;
+    }
+}
+class Stack 
+{
+    private LinkedList<Integer> stack;
+    
+    public Stack() {
+        stack = new LinkedList<>();
+    }
+    
+    public void push(int n) {
+        stack.addLast(n);
+    }
+    public int pop() {
+        return stack.removeLast();
+    }
+    public int peek() {
+        return stack.getLast();
+    }
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
