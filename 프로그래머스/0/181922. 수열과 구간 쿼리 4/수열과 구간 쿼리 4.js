4c2e4260bfc177d9fd1df8fe800a7c66a19e23ad
ChaJunWoo18 <105590071+ChaJunWoo18@users.noreply.github.com>
2024-11-16 02:35:36 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 281.88 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,12 @@
+function solution(arr, queries) {
+    var answer = [];
+    for(let q of queries) {
+        let [s, e, k] = q;
+        for(let i=s;i<=e;i++) {
+            if(i % k == 0)
+                arr[i] += 1
+        }
+        
+    }
+    return arr;
+}
